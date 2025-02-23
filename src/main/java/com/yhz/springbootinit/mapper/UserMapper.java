47df2b76a0874e0cbdbeeaf2d590b7d1fe456ec5
yhz1651 <572274170@qq.com>
2025-02-23 18:59:47 +0800

    Initial commit

---
+++ src/main/java/com/yhz/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yhz.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yhz.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
